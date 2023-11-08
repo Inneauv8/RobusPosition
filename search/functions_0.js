7f03463f569c37e56052fab1924abcecebee7fff
Mathieu52 <Mathieu52@users.noreply.github.com>
2023-11-08 17:42:23 +0000

    deploy: 75106f842d76a7bbba0999e6378ea190bda716cc

--- search/functions_0.js
+++ search/functions_0.js
@@ -4,5 +4,6 @@ var searchData=
   ['getfollowangularvelocityscale_1',['getFollowAngularVelocityScale',['../namespaceRobotPosition.html#aaf7bade92a858e73ce6d0ff78d347c8f',1,'RobotPosition']]],
   ['getfollowvelocity_2',['getFollowVelocity',['../namespaceRobotPosition.html#adaaaa273f70955e2261144833921266f',1,'RobotPosition']]],
   ['getorientation_3',['getOrientation',['../namespaceRobotPosition.html#a11f83b04ab81bbbcba2ed6227d7293cb',1,'RobotPosition']]],
-  ['getposition_4',['getPosition',['../namespaceRobotPosition.html#afc2b374da4f3ea1c6e4a5630e0c7641b',1,'RobotPosition']]]
+  ['getposition_4',['getPosition',['../namespaceRobotPosition.html#afc2b374da4f3ea1c6e4a5630e0c7641b',1,'RobotPosition']]],
+  ['gettarget_5',['getTarget',['../namespaceRobotPosition.html#adf25851da4bccabd6b609eddfc8fd76e',1,'RobotPosition']]]
 ];
